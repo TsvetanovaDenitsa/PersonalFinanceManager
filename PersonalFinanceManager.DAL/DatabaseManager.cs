591f390509d7fde511d62cd29971a1b0d48ea5e2
tsvet <tsvet@DENICA>
2026-05-16 18:17:14 +0300

    Implemented database connection manager

---
+++ PersonalFinanceManager.DAL/DatabaseManager.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace PersonalFinanceManager.DAL;
+
+public sealed class DatabaseManager
+{
+    private readonly string _connectionString;
+
+    public DatabaseManager(string? connectionString = null)
+    {
+        _connectionString = connectionString ?? @"Data Source=DENICA\SQLEXPRESS;Initial Catalog=PersonalFinanceDB;Integrated Security=True;Trust Server Certificate=True;";
+    }
+
+    public SqlConnection GetConnection()
+        => new SqlConnection(_connectionString);
+}
+
